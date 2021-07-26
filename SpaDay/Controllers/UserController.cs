4ef73fbb58ba386575b62d0c68e037186783bfdc
Patrick K <pk10534@hotmail.com>
2021-07-26 13:50:27 -0400

    Final

---
+++ SpaDay/Controllers/UserController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SpaDay.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SpaDay.Controllers
+{
+    public class UserController : Controller
+    {
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [Route("user/add")] ///user
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+        //[HttpGet]
+        [HttpPost]
+        public IActionResult SubmitAddUserForm(User newUser, string verify)
+        {
+            if (newUser.Password == verify)
+            {
+                ViewBag.NewUser = newUser;
+                return View("Index");
+            }
+            else
+            {
+                ViewBag.NewUsername = newUser.Username;
+                ViewBag.NewEmail= newUser.Email;
+                ViewBag.Error = "Error: Passwords do not match";
+                return View("Add");
+            }
+        }
+    }
+}
